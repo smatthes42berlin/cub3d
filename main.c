61b3e7546b9bcd7ff68fd290b38b646b64e7e5bc
Steffen Matthes <smatthes@student.42berlin.de>
2024-04-05 15:50:38 +0200

    combining

--- main.c
+++ main.c
@@ -5,12 +5,9 @@ int	main(int argc, char *argv[])
 	t_main_data	main_data;
 
 	init_main_data(&main_data, argc, argv);
-
 	parse_main(&main_data);
-
 	// check correct number of args
-	// 
-
+	//
 	return (0);
 }
 
@@ -21,6 +18,12 @@ int	init_main_data(t_main_data *main_data, int argc, char *argv[])
 	main_data->map.texture_east = NULL;
 	main_data->map.texture_north = NULL;
 	main_data->map.texture_south = NULL;
+	main_data->map.color_ceiling[0] = 0;
+	main_data->map.color_ceiling[1] = 0;
+	main_data->map.color_ceiling[2] = 0;
+	main_data->map.color_floor[0] = 0;
+	main_data->map.color_floor[1] = 0;
+	main_data->map.color_floor[2] = 0;
 	main_data->map.texture_west = NULL;
 	main_data->map.map = NULL;
 	return (0);
