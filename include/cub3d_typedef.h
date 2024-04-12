0c4e41a76b71266b3fb2f183520898334fcb6e61
Friedrich Szendzielorz <fszendzi@student.42berlin.de>
2024-04-12 17:20:13 +0200

    drawing map & player

--- include/cub3d_typedef.h
+++ include/cub3d_typedef.h
@@ -3,18 +3,29 @@
 
 #define FALSE 0
 #define TRUE 1
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
+
+# define PI 3.1415926535
+# define TWO_PI 6.28318530
+
+# define TILE_SIZE 64
+# define MAP_NUM_ROWS 13
+# define MAP_NUM_COLS 20
+
+#define WINDOW_WIDTH (MAP_NUM_COLS * TILE_SIZE)
+#define WINDOW_HEIGHT (MAP_NUM_ROWS * TILE_SIZE)
+
+#define FOV_ANGLE (60 * (PI / 180))
+
+#define NUM_RAYS WINDOW_WIDTH
+#define MINIMAP_SCALE_FACTOR 1.0
+
+//# define PI2 PI / 2
+//# define PI3 3 * PI / 2
+//# define DR 0.0174533
+//# define NUM_RAYS 60
+// # define FOV (60 * DR)
 
 # define REM_EMPTY_LINES_MAP_FILE false
-# define MAX_TILES 64
-# define PI 3.1415926535
-# define PI2 PI / 2
-# define PI3 3 * PI / 2
-# define DR 0.0174533
-# define NUM_RAYS 60
-# define FOV (60 * DR)
-
 // 111111111111999
 // 100000000001111
 // 100000000000001
@@ -23,7 +34,6 @@
 
 typedef struct s_map
 {
-	int					tiles[MAX_TILES];
 	int					width;
 	int					height;
 	int size; // size of each tile
@@ -39,13 +49,15 @@ typedef struct s_map
 
 typedef struct s_player
 {
-	int					x;
-	int					y;
-	int					size;
-	int					color;
-	float				angle;
-	float				dx;
-	float				dy;
+	float					x;
+	float					y;
+	float					size;
+	float					color;
+	int						turn_direction;
+	int						walk_direction;
+	float 					rotation_angle;
+	float 					walk_speed;
+	float                   turn_speed;
 }						t_player;
 
 typedef struct s_window
--- src/raycast/raycast.c
+++ src/raycast/raycast.c
@@ -1,5 +1,22 @@
 #include "cub3d.h"
 
+int map[MAP_NUM_ROWS][MAP_NUM_COLS] =
+	{
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+	};
+
 void destroy_window(t_main_data *data)
 {
 	t_xvar	*xvar;
@@ -44,17 +61,100 @@ int keys(int key, void *param)
     return 0;
 }
 
+
+void update(t_main_data *data)
+{
+
+	data->player.x += 1;
+	//data->player.y += 1;
+
+}
+
+void draw_tile(int *addr, int tile_x, int tile_y, int tile_color, t_main_data *data)
+{
+    int y = 0; 
+	int x;
+	int pixel_index;
+    while (y < TILE_SIZE)
+	{
+		x = 0;
+        while (x < TILE_SIZE)
+		{
+            pixel_index = ((tile_y + y) * data->w.width + (tile_x + x));
+            if (pixel_index < data->w.width * data->w.height)
+			{
+                addr[pixel_index] = tile_color;
+            }
+			x++;
+        }
+		y++;
+    }
+}
+
+void draw_map(t_main_data *data, int *addr)
+{
+    int i = 0;
+	int j;
+    while (i < MAP_NUM_ROWS)
+	{
+		j = 0;
+        while (j < MAP_NUM_COLS)
+		{
+            int tile_x = j * TILE_SIZE ;
+            int tile_y = i * TILE_SIZE;
+            int tile_color = map[i][j] != 0 ? 0xFFFFFF : 0x000000;
+            draw_tile(addr,
+					tile_x * MINIMAP_SCALE_FACTOR,
+					tile_y * MINIMAP_SCALE_FACTOR,
+					tile_color, data);
+			j++;
+        }
+		i++;
+    }
+}
+
+
+void draw_player(t_main_data *data, int *addr)
+{
+    int y = 0;
+    int scaled_size = data->player.size * MINIMAP_SCALE_FACTOR; // Scale the size of the player
+    while (y < scaled_size) // Use scaled size
+    {
+        int x = 0;
+        while (x < scaled_size) // Use scaled size
+        {
+            // Calculate the scaled position of the player
+            int scaled_x = data->player.x * MINIMAP_SCALE_FACTOR + x;
+            int scaled_y = data->player.y * MINIMAP_SCALE_FACTOR + y;
+
+            // Ensure we are within the window boundaries
+            if (scaled_x >= 0 && scaled_x < data->w.width && scaled_y >= 0 && scaled_y < data->w.height)
+            {
+                int pixel_index = scaled_y * data->w.width + scaled_x;
+                addr[pixel_index] = data->player.color;
+            }
+            x++;
+        }
+        y++;
+    }
+}
+
+
+void render(t_main_data *data, int *addr)
+{
+	draw_map(data, addr);
+	draw_player(data, addr);
+
+}
+
+
 int draw(t_main_data *data)
 {
 	void *frame = mlx_new_image(data->w.mlx, data->w.width, data->w.height);
-    //int *addr = (int *)mlx_get_data_addr(frame, &(int){32}, &(int){data->w.width * 4}, &(int){0});
-	//draw_map(data, addr);
-	//draw_grid_lines(data, addr);
-    //draw_player(data, addr);
-	//draw_player_direction(data, addr);
-	//draw_horizontal_rays(data, addr);
-	//draw_vertical_rays(data, addr);
-	//draw_rays1(data, addr);
+	
+    int *addr = (int *)mlx_get_data_addr(frame, &(int){32}, &(int){data->w.width * 4}, &(int){0});
+	//update(data);
+	render(data, addr);
 	mlx_put_image_to_window(data->w.mlx, data->w.mlx_win, frame, 0, 0);
 	mlx_destroy_image(data->w.mlx, frame);
 	return 0;
@@ -73,15 +173,34 @@ int initialize_window(t_main_data *data)
 	return TRUE;
 }
 
+void init_player(t_main_data *data)
+{
+	data->player.x = WINDOW_WIDTH / 2;
+    data->player.y = WINDOW_HEIGHT / 2;
+	data->player.size = 10;
+	data->player.color = 0xFFFF00;
+	data->player.turn_direction = 0;
+	data->player.walk_direction = 0;
+	data->player.rotation_angle = PI / 2;
+	data->player.walk_speed = 100;
+	data->player.turn_speed = 45 * (PI / 180);
+}
+
+
+void setup(t_main_data *data)
+{
+	init_player(data);
+}
+
 int init(t_main_data *data)
 {
-	//setup();
-
     if (!initialize_window(data))
 	{
         destroy_window(data);
         return FALSE;
     }
+
+	setup(data);
 
 	mlx_hook(data->w.mlx_win, 2, 1L<<0, keys, data);
 	mlx_hook(data->w.mlx_win, 17, 0, close_window, data);
