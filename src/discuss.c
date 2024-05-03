18556babe450b3d80ae31bbf33537e4cd9f52b17
Steffen Matthes <smatthes@student.42berlin.de>
2024-05-03 15:06:13 +0200

    norminette

--- src/discuss.c
+++ src/discuss.c
@@ -1,16 +1,16 @@
-- passed with 81 mcutura
-- idnt pass with 79 slombard
-- ask Friedrich about how to handel tex path parsing, whitespaces
-- talk about possible size limit of map
+// - passed with 81 mcutura
+// - idnt pass with 79 slombard
+// - ask Friedrich about how to handel tex path parsing, whitespaces
+// - talk about possible size limit of map
 
-- reaplce not defined with -
-- replace any minus in input with ?
-- evtl. ignore escaped quotes when checking for quotes
-- put all directions in the same textures
+// - reaplce not defined with -
+// - replace any minus in input with ?
+// - evtl. ignore escaped quotes when checking for quotes
+// - put all directions in the same textures
 
-- derive rotation angle from start pos for friedrich
-- remove discuss from repo, remove main from libft
+// - derive rotation angle from start pos for friedrich
+// - remove discuss from repo, remove main from libft
 
-- add 42 header
-- one question regarding suctracting -1 when going to next grid point
-- textures werden scheinbar falsch herum gerendert?
+// - add 42 header
+// - one question regarding suctracting -1 when going to next grid point
+// - textures werden scheinbar falsch herum gerendert?
